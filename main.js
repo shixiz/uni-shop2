aeafe969653dac6d7754c78f4ad786940a6a9958
shixiz <shixi12022@163.com>
2023-12-02 01:25:03 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http=$http
+$http.baseUrl='https://www.uinav.com'
+
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:"数据加载中..."
+  })
+}
+// 响应拦截器
+$http.afterRequest=function(){
+  uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg=function(title="数据请求失败",duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:"none"
+  })
+}
 
 Vue.config.productionTip = false
 
