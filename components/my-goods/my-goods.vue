de316490fb3b3cae281731069d314aa0f99d40d2
shixiz <shixi12022@163.com>
2023-12-04 19:23:11 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,73 @@
+<template>
+  <view>
+    <view class="goods-item">
+      <view class="goods-item-left">
+        <image :src="goods.goods_small_logo||defaultPic" class="goods-pic"></image>
+      </view>
+      <view class="goods-item-right">
+        <view class="goods-name"> {{goods.goods_name}} </view>
+        <view class="goods_info_box">
+          <view class="goods_price"> ￥{{goods.goods_price |toFixed}} </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      },
+    },
+    data() {
+      return {
+        // 默认的图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    filters: {
+      toFixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .goods-item-left {
+      margin-left: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+      }
+
+      .goods_info_box {
+        .goods_price {
+          font-size: 16px;
+          color: #C00000;
+        }
+      }
+    }
+  }
+</style>
--- subpackage/goods_list/goods_list.vue
+++ subpackage/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
-  <view>
-    goods_list
+  <view class="all">
+    <view class="goods-list">
+      <view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +12,64 @@
   export default {
     data() {
       return {
-        
+        // 请求参数对象
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量
+        total: 0,
+       isLoading:false
+      
       };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList(cb) {
+        this.isLoading=true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.isLoading=false
+        cb&&cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goodsList = [...this.goodsList,...res.message.goods]
+        this.total = res.message.total
+        console.log(this.goodsList);
+      },
+      gotoDetail(goods){
+        uni.navigateTo({
+          url:'/subpackage/goods_detail/goods_detail?gooos_id'+goods.goods_id
+        })
+      }
+    },
+    onReachBottom(){
+      if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完毕')
+      // 让页码自增+1  
+      if(this.isLoading==true) return
+      this.queryObj.pagenum++
+      this.getGoodsList()
+    },
+    onPullDownRefresh(){
+      // 重置关键数据
+      this.queryObj.pagenum=1
+      this.goodsList=[]
+      this.total=0
+      this.isLoading=false
+      this.getGoodsList(()=>uni.stopPullDownRefresh())
     }
   }
 </script>
 
 <style lang="scss">
-
+ 
 </style>
